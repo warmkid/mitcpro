290a06fa9113afacb12d8a2f9d379add9acc8b04
hezikuangxiang <heyifan940611@163.com>
2017-06-07 23:37:20 +0800

    AddExtructionProcessClassStep2-6
    AddExtructionProcessClassStep2-6

---
+++ mySystem/mySystem/Blow/Process/ExtructionCheckBeforePowerStep2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mySystem.Blow.Chart
+{
+    public partial class ExtructionCheckBeforePowerStep2 : Form
+    {
+        public ExtructionCheckBeforePowerStep2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ mySystem/mySystem/Blow/Process/ExtructionTransportRecordStep4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mySystem.Blow.Chart
+{
+    public partial class ExtructionTransportRecordStep4 : Form
+    {
+        public ExtructionTransportRecordStep4()
+        {
+            InitializeComponent();
+        }
+    }
+}
