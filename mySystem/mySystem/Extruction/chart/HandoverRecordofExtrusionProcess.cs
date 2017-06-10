760a56d196ed7194d5316555dbfb5fc75608b514
PoolinBUPT <snowhite163@bupt.edu.cn>
2017-06-10 22:57:08 +0800

    ~

---
+++ mySystem/mySystem/Extruction/chart/HandoverRecordofExtrusionProcess.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+//this form is about the 13th picture of the extrusion step 
+namespace mySystem.Extruction.Chart
+{
+    public partial class HandoverRecordofExtrusionProcess : Form
+    {
+        public HandoverRecordofExtrusionProcess()
+        {
+            InitializeComponent();
+            
+            //this part to add the confirm items
+            this.ltbConformItem.Items.Add("Id\tContent");
+            this.ltbConformItem.Items.Add("1\titem1");
+            this.ltbConformItem.Items.Add("2\titem2");
+            this.ltbConformItem.Items.Add("3\titem3");
+            this.ltbConformItem.Items.Add("4\titem4");
+            this.ltbConformItem.Items.Add("5\titem5");
+            this.ltbConformItem.Items.Add("6\titem6");
+            this.ltbConformItem.Items.Add("7\titem7");
+            this.ltbConformItem.Items.Add("8\titem8");
+            this.ltbConformItem.Items.Add("9\titem9");
+            this.ltbConformItem.Items.Add("10\titem10");
+            this.ltbConformItem.Items.Add("11\titem11");
+            this.ltbConformItem.Items.Add("12\titem12");
+            this.ltbConformItem.Items.Add("13\titem13");
+            this.ltbConformItem.Items.Add("14\titem14");
+            
+        }
+
+        private void btnDefault_Click(object sender, EventArgs e)
+        {
+            //this part automatically fill the blanks
+            this.rdbYes1d.Checked = true;
+            this.rdbYes1n.Checked = true;
+            this.rdbYes2d.Checked = true;
+            this.rdbYes2n.Checked = true;
+            this.rdbYes3d.Checked = true;
+            this.rdbYes3n.Checked = true;
+            this.rdbYes4d.Checked = true;
+            this.rdbYes4n.Checked = true;
+            this.rdbYes5d.Checked = true;
+            this.rdbYes5n.Checked = true;
+            this.rdbYes6d.Checked = true;
+            this.rdbYes6n.Checked = true;
+            this.rdbYes7d.Checked = true;
+            this.rdbYes7n.Checked = true;
+            this.rdbYes8d.Checked = true;
+            this.rdbYes8n.Checked = true;
+            this.rdbYes9d.Checked = true;
+            this.rdbYes9n.Checked = true;
+            this.rdbYes10d.Checked = true;
+            this.rdbYes10n.Checked = true;
+            this.rdbYes11d.Checked = true;
+            this.rdbYes11n.Checked = true;
+            this.rdbYes12d.Checked = true;
+            this.rdbYes12n.Checked = true;
+            this.rdbYes13d.Checked = true;
+            this.rdbYes13n.Checked = true;
+            this.rdbYes14d.Checked = true;
+            this.rdbYes14n.Checked = true;
+            this.txbBatchNoD.Text = "111";
+            this.txbBatchNoN.Text = "112";
+            this.txbAmountsD.Text = "100";
+            this.txbAmountsN.Text = "100";
+            this.txbCodeD.Text = "10000";
+            this.txbCodeN.Text = "10000";
+            this.txbAbnormalD.Text = "no";
+            this.txbAbnormalN.Text = "no";
+            this.txbHandinD.Text = "wang";
+            this.txbHandinN.Text = "zhang";
+            this.txbTakeinD.Text = "sun";
+            this.txbTakeinN.Text = "li";
+            this.dtpDay.Format = DateTimePickerFormat.Time;
+            this.dtpDay.Value = DateTime.Now;
+            this.dtpNight.Format = DateTimePickerFormat.Time;
+            this.dtpNight.Value = DateTime.Now;
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int[,] status = new int[2, 14] { { Convert.ToInt16(rdbYes1d.Checked), Convert.ToInt16(rdbYes2d.Checked), Convert.ToInt16(rdbYes3d.Checked), Convert.ToInt16(rdbYes4d.Checked), Convert.ToInt16(rdbYes5d.Checked), Convert.ToInt16(rdbYes6d.Checked), Convert.ToInt16(rdbYes7d.Checked), Convert.ToInt16(rdbYes8d.Checked), Convert.ToInt16(rdbYes9d.Checked), Convert.ToInt16(rdbYes10d.Checked), Convert.ToInt16(rdbYes11d.Checked), Convert.ToInt16(rdbYes12d.Checked), Convert.ToInt16(rdbYes13d.Checked), Convert.ToInt16(rdbYes14d.Checked) }, { Convert.ToInt16(rdbYes1n.Checked), Convert.ToInt16(rdbYes2n.Checked), Convert.ToInt16(rdbYes3n.Checked), Convert.ToInt16(rdbYes4n.Checked), Convert.ToInt16(rdbYes5n.Checked), Convert.ToInt16(rdbYes6n.Checked), Convert.ToInt16(rdbYes7n.Checked), Convert.ToInt16(rdbYes8n.Checked), Convert.ToInt16(rdbYes9n.Checked), Convert.ToInt16(rdbYes10n.Checked), Convert.ToInt16(rdbYes11n.Checked), Convert.ToInt16(rdbYes12n.Checked), Convert.ToInt16(rdbYes13n.Checked), Convert.ToInt16(rdbYes14n.Checked) } };
+            MessageBox.Show("first 7 stutus\t"+status[0,0]+status[0,1]+status[0,2]+status[0,3]+status[0,4]+status[0,5]+status[0,6]);
+        }
+
+    }
+}
---
+++ mySystem/mySystem/Extruction/chart/RunningRecordofFeedingUnit.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace mySystem.Extruction.Chart
+{
+    partial class RunningRecordofFeedingUnit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbDateturn = new System.Windows.Forms.Label();
+            this.lbChecktm = new System.Windows.Forms.Label();
+            this.lbEngine = new System.Windows.Forms.Label();
+            this.lbValve = new System.Windows.Forms.Label();
+            this.lbMaterial = new System.Windows.Forms.Label();
+            this.lbAlert = new System.Windows.Forms.Label();
+            this.lbSolve = new System.Windows.Forms.Label();
+            this.cmbEngine = new System.Windows.Forms.ComboBox();
+            this.cmbValve = new System.Windows.Forms.ComboBox();
+            this.cmbMaterial = new System.Windows.Forms.ComboBox();
+            this.cmbAlert = new System.Windows.Forms.ComboBox();
+            this.cmbSolve = new System.Windows.Forms.ComboBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.brnDefault = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.ltbShow = new System.Windows.Forms.ListBox();
+            this.dtpHour = new System.Windows.Forms.DateTimePicker();
+            this.btnrRemoveItem = new System.Windows.Forms.Button();
+            this.rdbDay = new System.Windows.Forms.RadioButton();
+            this.rdbNight = new System.Windows.Forms.RadioButton();
+            this.txbCheckman = new System.Windows.Forms.TextBox();
+            this.txbRecheckman = new System.Windows.Forms.TextBox();
+            this.lbCheckman = new System.Windows.Forms.Label();
+            this.lbRecheckman = new System.Windows.Forms.Label();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbDateturn
+            // 
+            this.lbDateturn.AutoSize = true;
+            this.lbDateturn.Location = new System.Drawing.Point(12, 45);
+            this.lbDateturn.Name = "lbDateturn";
+            this.lbDateturn.Size = new System.Drawing.Size(59, 12);
+            this.lbDateturn.TabIndex = 0;
+            this.lbDateturn.Text = "date turn";
+            // 
+            // lbChecktm
+            // 
+            this.lbChecktm.AutoSize = true;
+            this.lbChecktm.Location = new System.Drawing.Point(129, 45);
+            this.lbChecktm.Name = "lbChecktm";
+            this.lbChecktm.Size = new System.Drawing.Size(65, 12);
+            this.lbChecktm.TabIndex = 1;
+            this.lbChecktm.Text = "check time";
+            // 
+            // lbEngine
+            // 
+            this.lbEngine.AutoSize = true;
+            this.lbEngine.Location = new System.Drawing.Point(227, 45);
+            this.lbEngine.Name = "lbEngine";
+            this.lbEngine.Size = new System.Drawing.Size(83, 12);
+            this.lbEngine.TabIndex = 2;
+            this.lbEngine.Text = "engine status";
+            // 
+            // lbValve
+            // 
+            this.lbValve.AutoSize = true;
+            this.lbValve.Location = new System.Drawing.Point(328, 45);
+            this.lbValve.Name = "lbValve";
+            this.lbValve.Size = new System.Drawing.Size(77, 12);
+            this.lbValve.TabIndex = 3;
+            this.lbValve.Text = "valve status";
+            // 
+            // lbMaterial
+            // 
+            this.lbMaterial.AutoSize = true;
+            this.lbMaterial.Location = new System.Drawing.Point(421, 45);
+            this.lbMaterial.Name = "lbMaterial";
+            this.lbMaterial.Size = new System.Drawing.Size(95, 12);
+            this.lbMaterial.TabIndex = 4;
+            this.lbMaterial.Text = "material status";
+            // 
+            // lbAlert
+            // 
+            this.lbAlert.AutoSize = true;
+            this.lbAlert.Location = new System.Drawing.Point(538, 45);
+            this.lbAlert.Name = "lbAlert";
+            this.lbAlert.Size = new System.Drawing.Size(65, 12);
+            this.lbAlert.TabIndex = 5;
+            this.lbAlert.Text = "alert flag";
+            // 
+            // lbSolve
+            // 
+            this.lbSolve.AutoSize = true;
+            this.lbSolve.Location = new System.Drawing.Point(625, 45);
+            this.lbSolve.Name = "lbSolve";
+            this.lbSolve.Size = new System.Drawing.Size(65, 12);
+            this.lbSolve.TabIndex = 6;
+            this.lbSolve.Text = "solve falg";
+            // 
+            // cmbEngine
+            // 
+            this.cmbEngine.FormattingEnabled = true;
+            this.cmbEngine.Location = new System.Drawing.Point(229, 73);
+            this.cmbEngine.Name = "cmbEngine";
+            this.cmbEngine.Size = new System.Drawing.Size(57, 20);
+            this.cmbEngine.TabIndex = 7;
+            // 
+            // cmbValve
+            // 
+            this.cmbValve.FormattingEnabled = true;
+            this.cmbValve.Location = new System.Drawing.Point(330, 73);
+            this.cmbValve.Name = "cmbValve";
+            this.cmbValve.Size = new System.Drawing.Size(57, 20);
+            this.cmbValve.TabIndex = 8;
+            // 
+            // cmbMaterial
+            // 
+            this.cmbMaterial.FormattingEnabled = true;
+            this.cmbMaterial.Location = new System.Drawing.Point(423, 73);
+            this.cmbMaterial.Name = "cmbMaterial";
+            this.cmbMaterial.Size = new System.Drawing.Size(57, 20);
+            this.cmbMaterial.TabIndex = 9;
+            // 
+            // cmbAlert
+            // 
+            this.cmbAlert.FormattingEnabled = true;
+            this.cmbAlert.Location = new System.Drawing.Point(540, 73);
+            this.cmbAlert.Name = "cmbAlert";
+            this.cmbAlert.Size = new System.Drawing.Size(57, 20);
+            this.cmbAlert.TabIndex = 10;
+            // 
+            // cmbSolve
+            // 
+            this.cmbSolve.FormattingEnabled = true;
+            this.cmbSolve.Location = new System.Drawing.Point(627, 73);
+            this.cmbSolve.Name = "cmbSolve";
+            this.cmbSolve.Size = new System.Drawing.Size(57, 20);
+            this.cmbSolve.TabIndex = 11;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(3, 72);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(107, 21);
+            this.dtpDate.TabIndex = 12;
+            this.dtpDate.UseWaitCursor = true;
+            // 
+            // brnDefault
+            // 
+            this.brnDefault.Location = new System.Drawing.Point(169, 176);
+            this.brnDefault.Name = "brnDefault";
+            this.brnDefault.Size = new System.Drawing.Size(115, 22);
+            this.brnDefault.TabIndex = 13;
+            this.brnDefault.Text = "default optins";
+            this.brnDefault.UseVisualStyleBackColor = true;
+            this.brnDefault.Click += new System.EventHandler(this.brnDefault_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(345, 175);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // ltbShow
+            // 
+            this.ltbShow.FormattingEnabled = true;
+            this.ltbShow.ItemHeight = 12;
+            this.ltbShow.Location = new System.Drawing.Point(3, 218);
+            this.ltbShow.Name = "ltbShow";
+            this.ltbShow.Size = new System.Drawing.Size(681, 100);
+            this.ltbShow.TabIndex = 15;
+            // 
+            // dtpHour
+            // 
+            this.dtpHour.Location = new System.Drawing.Point(116, 72);
+            this.dtpHour.Name = "dtpHour";
+            this.dtpHour.Size = new System.Drawing.Size(107, 21);
+            this.dtpHour.TabIndex = 16;
+            this.dtpHour.UseWaitCursor = true;
+            // 
+            // btnrRemoveItem
+            // 
+            this.btnrRemoveItem.Location = new System.Drawing.Point(450, 175);
+            this.btnrRemoveItem.Name = "btnrRemoveItem";
+            this.btnrRemoveItem.Size = new System.Drawing.Size(75, 23);
+            this.btnrRemoveItem.TabIndex = 17;
+            this.btnrRemoveItem.Text = "remove item";
+            this.btnrRemoveItem.UseVisualStyleBackColor = true;
+            this.btnrRemoveItem.Click += new System.EventHandler(this.btnrRemoveItem_Click);
+            // 
+            // rdbDay
+            // 
+            this.rdbDay.AutoSize = true;
+            this.rdbDay.Location = new System.Drawing.Point(15, 115);
+            this.rdbDay.Name = "rdbDay";
+            this.rdbDay.Size = new System.Drawing.Size(41, 16);
+            this.rdbDay.TabIndex = 18;
+            this.rdbDay.TabStop = true;
+            this.rdbDay.Text = "day";
+            this.rdbDay.UseVisualStyleBackColor = true;
+            // 
+            // rdbNight
+            // 
+            this.rdbNight.AutoSize = true;
+            this.rdbNight.Location = new System.Drawing.Point(15, 137);
+            this.rdbNight.Name = "rdbNight";
+            this.rdbNight.Size = new System.Drawing.Size(53, 16);
+            this.rdbNight.TabIndex = 19;
+            this.rdbNight.TabStop = true;
+            this.rdbNight.Text = "night";
+            this.rdbNight.UseVisualStyleBackColor = true;
+            // 
+            // txbCheckman
+            // 
+            this.txbCheckman.Location = new System.Drawing.Point(210, 110);
+            this.txbCheckman.Name = "txbCheckman";
+            this.txbCheckman.Size = new System.Drawing.Size(100, 21);
+            this.txbCheckman.TabIndex = 20;
+            // 
+            // txbRecheckman
+            // 
+            this.txbRecheckman.Location = new System.Drawing.Point(210, 137);
+            this.txbRecheckman.Name = "txbRecheckman";
+            this.txbRecheckman.Size = new System.Drawing.Size(100, 21);
+            this.txbRecheckman.TabIndex = 21;
+            // 
+            // lbCheckman
+            // 
+            this.lbCheckman.AutoSize = true;
+            this.lbCheckman.Location = new System.Drawing.Point(151, 113);
+            this.lbCheckman.Name = "lbCheckman";
+            this.lbCheckman.Size = new System.Drawing.Size(53, 12);
+            this.lbCheckman.TabIndex = 22;
+            this.lbCheckman.Text = "checkman";
+            // 
+            // lbRecheckman
+            // 
+            this.lbRecheckman.AutoSize = true;
+            this.lbRecheckman.Location = new System.Drawing.Point(139, 140);
+            this.lbRecheckman.Name = "lbRecheckman";
+            this.lbRecheckman.Size = new System.Drawing.Size(65, 12);
+            this.lbRecheckman.TabIndex = 23;
+            this.lbRecheckman.Text = "recheckman";
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Location = new System.Drawing.Point(264, 21);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(185, 12);
+            this.lbTitle.TabIndex = 24;
+            this.lbTitle.Text = "Running Record of Feeding Unit";
+            // 
+            // RunningRecordofFeedingUnit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 344);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.lbRecheckman);
+            this.Controls.Add(this.lbCheckman);
+            this.Controls.Add(this.txbRecheckman);
+            this.Controls.Add(this.txbCheckman);
+            this.Controls.Add(this.rdbNight);
+            this.Controls.Add(this.rdbDay);
+            this.Controls.Add(this.btnrRemoveItem);
+            this.Controls.Add(this.dtpHour);
+            this.Controls.Add(this.ltbShow);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.brnDefault);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.cmbSolve);
+            this.Controls.Add(this.cmbAlert);
+            this.Controls.Add(this.cmbMaterial);
+            this.Controls.Add(this.cmbValve);
+            this.Controls.Add(this.cmbEngine);
+            this.Controls.Add(this.lbSolve);
+            this.Controls.Add(this.lbAlert);
+            this.Controls.Add(this.lbMaterial);
+            this.Controls.Add(this.lbValve);
+            this.Controls.Add(this.lbEngine);
+            this.Controls.Add(this.lbChecktm);
+            this.Controls.Add(this.lbDateturn);
+            this.Name = "RunningRecordofFeedingUnit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbDateturn;
+        private System.Windows.Forms.Label lbChecktm;
+        private System.Windows.Forms.Label lbEngine;
+        private System.Windows.Forms.Label lbValve;
+        private System.Windows.Forms.Label lbMaterial;
+        private System.Windows.Forms.Label lbAlert;
+        private System.Windows.Forms.Label lbSolve;
+        private System.Windows.Forms.ComboBox cmbEngine;
+        private System.Windows.Forms.ComboBox cmbValve;
+        private System.Windows.Forms.ComboBox cmbMaterial;
+        private System.Windows.Forms.ComboBox cmbAlert;
+        private System.Windows.Forms.ComboBox cmbSolve;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Button brnDefault;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox ltbShow;
+        private System.Windows.Forms.DateTimePicker dtpHour;
+        private System.Windows.Forms.Button btnrRemoveItem;
+        private System.Windows.Forms.RadioButton rdbDay;
+        private System.Windows.Forms.RadioButton rdbNight;
+        private System.Windows.Forms.TextBox txbCheckman;
+        private System.Windows.Forms.TextBox txbRecheckman;
+        private System.Windows.Forms.Label lbCheckman;
+        private System.Windows.Forms.Label lbRecheckman;
+        private System.Windows.Forms.Label lbTitle;
+    }
+}
