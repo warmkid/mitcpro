c7a68d176c781a7c3c7b46e880fc8b181d159a0d
WIN-6\Administrator <823137140@qq.com>
2017-07-19 09:37:53 +0800

    指令界面

--- mySystem/mySystem/Process/CleanCut/Instru.Designer.cs
+++ mySystem/mySystem/Process/CleanCut/Instru.Designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Insid = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.facility = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -53,7 +49,13 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.bt查询插入 = new System.Windows.Forms.Button();
+            this.tb指令编号 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb设备编号 = new System.Windows.Forms.TextBox();
+            this.dtp开始生产日期 = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -63,39 +65,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(466, 32);
+            this.label1.Location = new System.Drawing.Point(347, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(231, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "清洁/分切 工序生产指令";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Insid,
-            this.facility,
-            this.date});
-            this.dataGridView1.Location = new System.Drawing.Point(73, 70);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(381, 63);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // Insid
-            // 
-            this.Insid.HeaderText = "指令编号";
-            this.Insid.Name = "Insid";
-            // 
-            // facility
-            // 
-            this.facility.HeaderText = "生产设备";
-            this.facility.Name = "facility";
-            // 
-            // date
-            // 
-            this.date.HeaderText = "计划生产日期";
-            this.date.Name = "date";
             // 
             // dataGridView2
             // 
@@ -244,11 +218,82 @@
             this.label5.TabIndex = 14;
             this.label5.Text = " 编制人：                     审批人：                     接收人";
             // 
+            // bt查询插入
+            // 
+            this.bt查询插入.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bt查询插入.Location = new System.Drawing.Point(313, 84);
+            this.bt查询插入.Name = "bt查询插入";
+            this.bt查询插入.Size = new System.Drawing.Size(84, 23);
+            this.bt查询插入.TabIndex = 26;
+            this.bt查询插入.Text = "查询/插入";
+            this.bt查询插入.UseVisualStyleBackColor = true;
+            // 
+            // tb指令编号
+            // 
+            this.tb指令编号.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb指令编号.Location = new System.Drawing.Point(177, 84);
+            this.tb指令编号.Name = "tb指令编号";
+            this.tb指令编号.Size = new System.Drawing.Size(96, 23);
+            this.tb指令编号.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(54, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 14);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "生产指令编号";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(440, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 14);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "生产设备编号";
+            // 
+            // tb设备编号
+            // 
+            this.tb设备编号.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb设备编号.Location = new System.Drawing.Point(542, 84);
+            this.tb设备编号.Name = "tb设备编号";
+            this.tb设备编号.Size = new System.Drawing.Size(87, 23);
+            this.tb设备编号.TabIndex = 27;
+            // 
+            // dtp开始生产日期
+            // 
+            this.dtp开始生产日期.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dtp开始生产日期.Location = new System.Drawing.Point(787, 84);
+            this.dtp开始生产日期.Name = "dtp开始生产日期";
+            this.dtp开始生产日期.Size = new System.Drawing.Size(136, 23);
+            this.dtp开始生产日期.TabIndex = 30;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(670, 88);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 14);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "开始生产日期";
+            // 
             // Instru
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1016, 741);
+            this.Controls.Add(this.dtp开始生产日期);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tb设备编号);
+            this.Controls.Add(this.bt查询插入);
+            this.Controls.Add(this.tb指令编号);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
@@ -261,13 +306,11 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Instru";
             this.Text = "Instru";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -281,10 +324,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Insid;
-        private System.Windows.Forms.DataGridViewTextBoxColumn facility;
-        private System.Windows.Forms.DataGridViewTextBoxColumn date;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
@@ -305,5 +344,12 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button bt查询插入;
+        private System.Windows.Forms.TextBox tb指令编号;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb设备编号;
+        private System.Windows.Forms.DateTimePicker dtp开始生产日期;
+        private System.Windows.Forms.Label label8;
     }
 }
