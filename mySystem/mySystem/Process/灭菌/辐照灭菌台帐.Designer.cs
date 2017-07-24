5d5f5ab61b6911f1f979e9bf43ddcc4f73367a6e
kr0920 <13521903230@163.com>
2017-07-24 15:47:51 +0800

    上传CS制袋数据库

--- mySystem/mySystem/Process/灭菌/辐照灭菌台帐.Designer.cs
+++ mySystem/mySystem/Process/灭菌/辐照灭菌台帐.Designer.cs
@@ -31,6 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.b打印 = new System.Windows.Forms.Button();
+            this.bt添加 = new System.Windows.Forms.Button();
+            this.bt保存 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,16 +62,38 @@
             this.b打印.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.b打印.Location = new System.Drawing.Point(663, 322);
             this.b打印.Name = "b打印";
-            this.b打印.Size = new System.Drawing.Size(75, 23);
+            this.b打印.Size = new System.Drawing.Size(75, 41);
             this.b打印.TabIndex = 2;
             this.b打印.Text = "打印";
             this.b打印.UseVisualStyleBackColor = true;
+            // 
+            // bt添加
+            // 
+            this.bt添加.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bt添加.Location = new System.Drawing.Point(422, 326);
+            this.bt添加.Name = "bt添加";
+            this.bt添加.Size = new System.Drawing.Size(75, 37);
+            this.bt添加.TabIndex = 3;
+            this.bt添加.Text = "添加";
+            this.bt添加.UseVisualStyleBackColor = true;
+            // 
+            // bt保存
+            // 
+            this.bt保存.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bt保存.Location = new System.Drawing.Point(534, 326);
+            this.bt保存.Name = "bt保存";
+            this.bt保存.Size = new System.Drawing.Size(75, 37);
+            this.bt保存.TabIndex = 4;
+            this.bt保存.Text = "保存";
+            this.bt保存.UseVisualStyleBackColor = true;
             // 
             // 辐照灭菌台帐
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(770, 361);
+            this.ClientSize = new System.Drawing.Size(770, 403);
+            this.Controls.Add(this.bt保存);
+            this.Controls.Add(this.bt添加);
             this.Controls.Add(this.b打印);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
@@ -86,5 +110,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button b打印;
+        private System.Windows.Forms.Button bt添加;
+        private System.Windows.Forms.Button bt保存;
     }
 }
