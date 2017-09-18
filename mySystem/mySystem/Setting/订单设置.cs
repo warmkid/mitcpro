49ed386ff860fb43c51d714284a76ab21c93133e
karon1988 <liuyang8660@163.com>
2017-09-18 15:33:26 +0800

    删除时也有问题，注释掉设置序号的函数

--- mySystem/mySystem/Setting/订单设置.cs
+++ mySystem/mySystem/Setting/订单设置.cs
@@ -360,7 +360,7 @@ namespace mySystem.Setting
             da存货档案.Update((DataTable)bs存货档案.DataSource);
             dt存货档案.Clear();
             da存货档案.Fill(dt存货档案);
-            setDataGridViewRowNums(this.dgv存货档案);
+            //setDataGridViewRowNums(this.dgv存货档案);
         }
 
         
