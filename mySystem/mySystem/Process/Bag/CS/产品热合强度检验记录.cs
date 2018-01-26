ba32eef359f5523c2f223b6df014623195e14647
MengBUPT <zmbupt@163.com>
2018-01-26 16:51:23 +0800

    CS+灭菌“审核通过”修改

--- mySystem/mySystem/Process/Bag/CS/产品热合强度检验记录.cs
+++ mySystem/mySystem/Process/Bag/CS/产品热合强度检验记录.cs
@@ -64,7 +64,10 @@ namespace mySystem.Process.Bag.CS
                 DataRow dr = dtOuter.NewRow();
                 dr = writeOuterDefault(dr);
                 dtOuter.Rows.Add(dr);
-                ((DataTable)bsOuter.DataSource).Rows[0]["审核是否通过"] = 0;
+                if (((DataTable)bsOuter.DataSource).Rows[0]["审核是否通过"] == DBNull.Value)
+                {
+                    ((DataTable)bsOuter.DataSource).Rows[0]["审核是否通过"] = 0;
+                }
                 daOuter.Update((DataTable)bsOuter.DataSource);
                 readOuterData();
                 outerBind();
