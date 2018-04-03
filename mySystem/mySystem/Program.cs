44aaa78e9688b7fdc5a6a18cac5817e14c7be045
karon1988 <liuyang8660@163.com>
2018-04-03 13:45:31 +0800

    所有的错误信息都有记录了

--- mySystem/mySystem/Program.cs
+++ mySystem/mySystem/Program.cs
@@ -14,9 +14,19 @@ namespace mySystem
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            catch (Exception e)
+            {
+                String dt = DateTime.Now.ToString("yyyy-MM-dd_hh-mm_ss");
+                MessageBox.Show( String.Format("出现错误，信息已记录至{0}.txt ",dt));
+                System.IO.File.WriteAllText(String.Format("{0}.txt ", dt), e.StackTrace);
+                System.Diagnostics.Process.Start("explorer.exe", ".");
+            }
         }
     }
 }
